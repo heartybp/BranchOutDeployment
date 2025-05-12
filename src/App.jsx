ad5a5b54883d3b1be80943cabeef20bc6fde721f
heartybp <parrenashearty@gmail.com>
2025-05-12 00:10:20 -0700

    change to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp.jsx";
 import Login from "./components/Login.jsx";
-
 import Home from "./Home.jsx";
 import Network from "./Network.jsx";
 import Forum from "./Forum.jsx";
@@ -13,29 +11,21 @@ import Loading from "./components/Loading.jsx";
 import Header from "./components/Header";
 // import AIResumeFeedback from "./components/AIResumeFeedback";
 
-// App.jsx is the entry point of a React application
-// Here, you define your main React component and render the initial structure of the user interface
-
-// components are listed in the components folder in src
-// client > src > components
-
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/loading" element={<Loading/>} />
-        <Route path="/onBoardingQuiz" element={<OnboardingQuiz/>} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/onBoardingQuiz" element={<OnboardingQuiz />} />
         <Route path="/home" element={<Home />} />
-
         <Route path="/network" element={<Network />} />
         <Route path="/forum" element={<Forum />} />
-
         <Route path="/profile" element={<Profile />} />
         <Route path="/resources" element={<Resources />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
